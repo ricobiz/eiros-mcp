55a252378a2670b0ae3f124a092447098b43f12d
ricobiz <132233825+ricobiz@users.noreply.github.com>
2025-12-02 23:07:32 +0700

    Update index.js

--- app/api/cmd/commands/index.js
+++ app/api/cmd/commands/index.js
@@ -2,9 +2,9 @@ import listFiles from "./ListFiles";
 import readFile from "./ReadFile";
 import pushFile from "./PushFile";
 import getStatus from "./GetStatus";
-import sendRadioMessage from "./SendRadioMessage";
-import queueTrack from "./QueueTrack";
-import log from "./log";
+import sendRadioMessage from "./sendRadioMessage";
+import queueTrack from "./Queuetrack";
+import log from "./Log";
 import tts from "./tts";
 
 export const handlers = {
